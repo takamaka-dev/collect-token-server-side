e4a8df3ea503ac6c975fd218c0c7112a5aa2d384
isacco <isacco.borsani@h2tcoin.com>
2023-01-24 12:54:21 +0100

    first commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,18 @@
 CREATE TABLE collecttokenserverschema.tokencollected (
-wallet_address VARCHAR(44) PRIMARY KEY
-
-
+    wallet_address VARCHAR(44),
+    challenge_id INTEGER AUTO_INCREMENT,
+    solution_string TEXT,
+    sent_challenge TEXT,
+    PRIMARY KEY(wallet_address, challenge_id)
 );
+
+CREATE TABLE collecttokenserverschema.pay_sent (
+    wallet_address VARCHAR(44) primary KEY,
+    number_of_pay INTEGER NOT NULL DEFAULT 0
+);
+
+-- il n_shards_requested sarà: 15 * n_pay * 2
+-- se wallet address not exists in collecttokenserverschema.pay_sent then n_shards_requested = 15
 
 ALTER TABLE IF EXISTS collecttokenserverschema.tokencollected
     OWNER to collecttokenserveruser;
